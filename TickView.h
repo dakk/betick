f5a17c1057332c816a3539c1d7133ec4cba602e2
Davide Gessa <gessadavide@gmail.com>
2015-07-02 11:39:57 +0200

    License switching

--- TickView.h
+++ TickView.h
@@ -1,8 +1,23 @@
 /*
- * Copyright (c) Casalinuovo Dario. All rights reserved.
- * Copyright (c) Davide Gessa. All rights reserved.
- * Distributed under the terms of the MIT License for non commercial use.
+ * Copyright (C) 2012-2015  Gessa Davide (gessadavide@gmail.com)
+ * Copyright (C) 2012-2015  Casalinuovo Dario (b.vitruvio@gmail.com)
  *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ */
+/* 
  * Authors:
  *		Davide Gessa, dak.linux@gmail.com
  *		Dario Casalinuovo
